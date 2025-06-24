2a7ebd02f055e727948997b6450e2cc5fe2a3e59
Jem Arnold <132770429+jemarnold@users.noreply.github.com>
2025-06-24 14:58:01 -0700

    Update signif_trailing.R

--- R/signif_trailing.R
+++ R/signif_trailing.R
@@ -1,27 +1,38 @@
 #' Print a numeric value as character with trailing zeroes
 #'
-#' Converts a numeric vector to a character vector to preserve trailing zeroes
+#' Converts a numeric to a character object to preserve trailing zeroes
 #' for display.
 #'
 #' @param x A numeric vector.
 #' @param digits An integer scalar specifying the number of digits to preserve.
 #' @param format Indicates how to treat `digits`. Either the desired number of
-#' digits after the decimal point (`format = "f"`) or significant digits
-#' (`format = "fg"`).
+#' digits after the decimal point (`format = "digits"`) or significant digits
+#' (`format = "signif"`).
 #'
 #' @return A character vector.
 #'
-#' @examples
-#' signif_trailing(c(1, 1.3, 1.35), digits = 2, format = "f")
-#' signif_trailing(c(1, 1.3, 1.35), digits = 2, format = "fg")
+#' @seealso [formatC()], [round()], [signif()]
 #'
+#' @examples
+#' x <- c(1, 1.3, 1.35)
+#' signif_trailing(x, digits = 2, format = "digits")
+#' signif_trailing(x, digits = 2, format = "signif")
+#'
+#' @keywords internal
 #' @export
-signif_trailing <- function(x, digits = 2, format = c("f", "fg")) {
-    ## format = "f" for decimal place; "fg" for signif
+signif_trailing <- function(
+        x,
+        digits = 2,
+        format = c("digits", "signif")
+) {
+    ## format = "digits" for decimal place; "signif" for signif
     format = match.arg(format)
+    format <- switch(format,
+                     "digits" = "f",
+                     "signif" = "fg")
 
     ## argument validation
-    if (!rlang::is_double(x)) {
+    if (!is.numeric(x)) {
         cli::cli_abort("{.arg x} must be a numeric vector.")
     }
     if (!rlang::is_integerish(digits)) {
@@ -30,8 +41,5 @@ signif_trailing <- function(x, digits = 2, format = c("f", "fg")) {
 
     ## remove trailing `.` or "NA"
     ## https://stackoverflow.com/a/35280610/15389854
-    gsub("\\.$|NA", "",
-         formatC(
-             x, digits = digits, format = format, flag = "#"))
+    gsub("\\.$|NA", "", formatC(x, digits = digits, format = format, flag = "#"))
 }
-
